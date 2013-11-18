484aba9b388dc653bfe81feb1ff941e4cf3a940c
Alex McTammany <apmctammany@gmail.com>
2013-11-18 16:58:21 -0500

    init checkin

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('mctApp', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
