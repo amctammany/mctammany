2c5501ed947165021eb5f48f31901d6aa43c3edc
Alex McTammany <apmctammany@gmail.com>
2013-11-20 19:54:41 -0500

    checkin before experiments

--- app/scripts/molecules/controllers/show.js
+++ app/scripts/molecules/controllers/show.js
@@ -5,15 +5,17 @@ angular.module('mctApp')
     $scope.moleculeName = $routeParams.name;
     if ($routeParams.name) {
       $scope.moleculeStore = MoleculeStore.get({name: $routeParams.name}, function (data) {
+        if (!data.molFile) {return;}
         $scope.molecule = new Molecule(data.name, data.molFile, $scope.ctx);
         $scope.name = $scope.molecule.name;
         $scope.molecule.parseMolFile();
         $scope.renderer = new Renderer($scope.molecule, 'moleculeCanvas');
-        $scope.renderer.animate();
-        //$scope.renderer.rotateTo(0.6, .6, 0.0);
+        //$scope.renderer.animate();
+        $scope.renderer.render();
+
       });
     } else {
-      $scope.molecule = new Molecule(false, false, $scope.ctx);
+      
     }
 
 
