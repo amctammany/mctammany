e8b391bc88761fdc2f84b6420904f64af719d8b4
Alex McTammany <apmctammany@gmail.com>
2013-12-12 22:11:53 -0500

    put maze into own service

--- app/scripts/experiments/controllers/maze.js
+++ app/scripts/experiments/controllers/maze.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('mctApp')
-  .controller('MazeCtrl', function ($scope) {
+  .controller('MazeCtrl', function ($scope, Maze) {
     $scope.rows = 10;
     $scope.columns = 10;
 
     $scope.mazes = ['hi', 'you7', 'foo'];
+
 
     var cellWidth, cellHeight;
 
@@ -14,190 +15,30 @@ angular.module('mctApp')
     var endFill = 'red';
     $scope.init = function () {
     
-      $scope.canvas = angular.element(document.getElementById('maze-canvas'))[0];
-      $scope.ctx = $scope.canvas.getContext('2d');
+      var canvas = angular.element(document.getElementById('maze-canvas'))[0];
 
-      $scope.canvas.width = 700;
-      $scope.canvas.height = 700;
+      canvas.width = 700;
+      canvas.height = 700;
 
+      $scope.maze = new Maze(canvas, $scope.columns, $scope.rows);
+      cellWidth = $scope.maze.cellWidth;
+      cellHeight = $scope.maze.cellHeight;
     };
 
 
-    function index (column, row) {
-      return column + (row * $scope.columns);
-    }
 
-    var Cell = function (column, row) {
-      this.column = column;
-      this.row = row;
-      this.fill = 'lightsteelblue';
-      this.left = false;
-      this.leftColor = 'black';
-      this.right = false;
-      this.rightColor = 'black';
-      this.top = false;
-      this.topColor = 'black';
-      this.bottom = false;
-      this.bottomColor = 'black';
-
-    };
-
-// Remove Wall {
-    Cell.prototype.removeWall = function (side) {
-      var neighbor;
-      if (side === 'left') {
-        neighbor = $scope.cells[index(this.column - 1, this.row)];
-        if (neighbor) {
-          this.left = this.left ? false : true;
-          neighbor.right = neighbor.right ? false : true;
-        }
-      }
-      if (side === 'right') {
-        neighbor = $scope.cells[index(this.column + 1, this.row)];
-        if (neighbor) {
-          this.right = this.right ? false : true;
-          neighbor.left = neighbor.left ? false : true;
-        }
-      }
-
-      if (side === 'top') {
-        neighbor = $scope.cells[index(this.column, this.row - 1)];
-        if (neighbor) {
-          this.top = this.top ? false : true;
-          neighbor.bottom = neighbor.bottom ? false : true;
-        }
-      }
-
-      if (side === 'bottom') {
-        neighbor = $scope.cells[index(this.column, this.row + 1)];
-        if (neighbor) {
-          this.bottom = this.bottom ? false : true;
-          neighbor.top = neighbor.top ? false : true;
-        }
-      }
-
-
-    };
-// }
-
-// Highlight Side {
-    Cell.prototype.highlightSide = function (side) {
-      var neighbor;
-      if (side === 'left') {
-        this.leftColor = 'red';
-        neighbor = $scope.cells[index(this.column - 1, this.row)];
-        if (neighbor) {
-          neighbor.rightColor = 'red';
-        }
-      }
-      if (side === 'right') {
-        this.rightColor = 'red';
-        neighbor = $scope.cells[index(this.column + 1, this.row)];
-        if (neighbor) {
-          neighbor.leftColor = 'red';
-        }
-      }
-
-      if (side === 'top') {
-        this.topColor = 'red';
-        neighbor = $scope.cells[index(this.column, this.row - 1)];
-        if (neighbor) {
-          neighbor.bottomColor = 'red';
-        }
-      }
-
-      if (side === 'bottom') {
-        this.bottomColor = 'red';
-        neighbor = $scope.cells[index(this.column, this.row + 1)];
-        if (neighbor) {
-          neighbor.topColor = 'red';
-        }
-      }
-
-    };
-// }
-
-// Draw {
-    Cell.prototype.draw = function (ctx) {
-      ctx.lineWidth = 1;
-      if (!this.left) {
-        ctx.strokeStyle = this.leftColor;
-        ctx.beginPath();
-        ctx.moveTo(this.column * cellWidth, this.row * cellHeight);
-        ctx.lineTo(this.column * cellWidth, (this.row + 1) * cellHeight);
-        ctx.closePath();
-        ctx.stroke();
-      }
-      if (!this.right) {
-        ctx.strokeStyle = this.rightColor;
-        ctx.beginPath();
-        ctx.moveTo((this.column + 1) * cellWidth, this.row * cellHeight);
-        ctx.lineTo((this.column + 1)* cellWidth, (this.row + 1) * cellHeight);
-        ctx.closePath();
-        ctx.stroke();
-      }
-      if (!this.top) {
-        ctx.strokeStyle = this.topColor;
-        ctx.beginPath();
-        ctx.moveTo(this.column * cellWidth, this.row * cellHeight);
-        ctx.lineTo((this.column + 1) * cellWidth, this.row * cellHeight);
-        ctx.closePath();
-        ctx.stroke();
-      }
-      if (!this.bottom) {
-        ctx.strokeStyle = this.bottomColor;
-        ctx.beginPath();
-        ctx.moveTo(this.column * cellWidth, (this.row + 1) * cellHeight);
-        ctx.lineTo((this.column + 1) * cellWidth, (this.row + 1) * cellHeight);
-        ctx.closePath();
-        ctx.stroke();
-      }
-      if ($scope.start === this) {
-        ctx.fillStyle = startFill;
-      } else if ($scope.end === this) {
-        ctx.fillStyle = endFill;
-      } else {
-        ctx.fillStyle = defaultFill;
-      }
-      ctx.beginPath();
-      ctx.fillRect(this.column * cellWidth + 1, this.row * cellHeight + 1, cellWidth - 2, cellHeight - 2);
-      ctx.closePath();
-
-    };
-
-// }
-
-
-
-    $scope.generateBlankMaze = function () {
-      var row, column, cell;
-      cellWidth = $scope.canvas.width / $scope.columns;
-      cellHeight = $scope.canvas.height / $scope.rows;
-
-      $scope.cells = [];
-      $scope.walls = [];
-
-      $scope.cells = new Array($scope.rows * $scope.columns);
-      for (row = 0; row < $scope.rows; ++row) {
-        for (column = 0; column < $scope.columns; ++column) {
-          cell = new Cell(column, row);
-          $scope.cells[index(column, row)] = cell;
-        }
-      }
-      draw();
-    };
 
 
     $scope.handleMouseDown = function (e) {
       var column = Math.floor(e.offsetX / cellWidth);
       var row = Math.floor(e.offsetY / cellHeight);
-      var cell = $scope.cells[index(column, row)];
-      if ($scope.startSelection) {
-        $scope.startSelection = false;
+      var cell = $scope.maze.findCell(column, row);
+      if ($scope.maze.startSelection) {
+        $scope.maze.startSelection = false;
         return;
       }
-      if ($scope.endSelection) {
-        $scope.endSelection = false;
+      if ($scope.maze.endSelection) {
+        $scope.maze.endSelection = false;
         return;
       }
       //cell.bottom = true;
@@ -214,22 +55,21 @@ angular.module('mctApp')
       };
       var side = $scope.findLowestDelta(deltas);
       cell.removeWall(side);
-      console.log(cell);
-      draw();
+      $scope.maze.draw();
     };
     $scope.handleMouseMove = function (e) {
       var column = Math.floor(e.offsetX / cellWidth);
       var row = Math.floor(e.offsetY / cellHeight);
-      var cell = $scope.cells[index(column, row)];
+      var cell = $scope.maze.findCell(column, row);
 
-      if ($scope.startSelection) {
-        $scope.start = cell;
+      if ($scope.maze.startSelection) {
+        $scope.maze.start = cell;
       }
-      if ($scope.endSelection) {
-        $scope.end = cell;
+      if ($scope.maze.endSelection) {
+        $scope.maze.end = cell;
       }
 
-      draw();
+      $scope.maze.draw();
     
     };
 
@@ -241,11 +81,11 @@ angular.module('mctApp')
     };
 
     $scope.addStart = function () {
-      $scope.startSelection = true;
+      $scope.maze.startSelection = true;
     };
 
     $scope.addEnd = function () {
-      $scope.endSelection = true;
+      $scope.maze.endSelection = true;
     };
     $scope.findLowestDelta = function (deltas) {
       var lowest = Object.keys(deltas).reduce(function (previous, key) {
--- app/scripts/experiments/services/maze.js
+++ app/scripts/experiments/services/maze.js
@@ -2,13 +2,184 @@
 
 angular.module('mctApp')
   .factory('Maze', function () {
-    var Maze = function (canvas) {
+
+    var maxRows, maxColumns;
+    var index = function index (column, row) {
+      return column + (row * maxRows);
+    };
+
+    var findLowestDelta = function (deltas) {
+      var lowest = Object.keys(deltas).reduce(function (previous, key) {
+        var d = deltas[key];
+        if (d < deltas[previous]) {
+          return key;
+        }
+        return previous;
+      });
+      return lowest;
+    };
+
     
+    var Maze = function (canvas, columns, rows) {
+      this.rows = rows;
+      this.columns = columns;
+      maxRows = rows;
+      maxColumns = columns;
+
+      this.defaultFill = 'lightsteelblue';
+      this.startFill = 'lightgreen';
+      this.endFill = 'red';
       if (canvas) {
         this.canvas = canvas;
         this.ctx = this.canvas.getContext('2d');
+
+        this.height = canvas.height;
+        this.width = canvas.width;
       }
+      this.reset();
     };
+
+    Maze.prototype.findCell = function (column, row) {
+      var cell = this.cells[index(column, row)];
+      return cell;
+    };
+
+    Maze.prototype.reset = function () {
+      var row, column, cell;
+      this.cellWidth = this.canvas.width / this.columns;
+      this.cellHeight = this.canvas.height / this.rows;
+
+
+      this.cells = new Array(this.rows * this.columns);
+      for (row = 0; row < this.rows; ++row) {
+        for (column = 0; column < this.columns; ++column) {
+          cell = this.addCell(column, row);
+          this.cells[index(column, row)] = cell;
+        }
+      }
+      this.draw();
+    };
+
+    Maze.prototype.addCell = function (column, row) {
+      var cell = new Cell(column, row);
+      cell.maze = this;
+      //this.cells[index(column, row)] = cell;
+      return cell;
+    };
+
+    Maze.prototype.draw = function () {
+      var ctx = this.ctx;
+      ctx.clearRect(0, 0, this.width, this.height);
+      this.cells.forEach(function (cell) {
+        cell.draw(ctx);
+      });
+    };
+
+    var Cell = function (column, row) {
+      this.column = column;
+      this.row = row;
+      this.fill = 'lightsteelblue';
+      this.left = false;
+      this.leftColor = 'black';
+      this.right = false;
+      this.rightColor = 'black';
+      this.top = false;
+      this.topColor = 'black';
+      this.bottom = false;
+      this.bottomColor = 'black';
+
+    };
+
+// Remove Wall {
+    Cell.prototype.removeWall = function (side) {
+      var neighbor;
+      var maze = this.maze;
+      if (side === 'left') {
+        neighbor = maze.cells[index(this.column - 1, this.row)];
+        if (neighbor) {
+          this.left = this.left ? false : true;
+          neighbor.right = neighbor.right ? false : true;
+        }
+      }
+      if (side === 'right') {
+        neighbor = maze.cells[index(this.column + 1, this.row)];
+        if (neighbor) {
+          this.right = this.right ? false : true;
+          neighbor.left = neighbor.left ? false : true;
+        }
+      }
+
+      if (side === 'top') {
+        neighbor = maze.cells[index(this.column, this.row - 1)];
+        if (neighbor) {
+          this.top = this.top ? false : true;
+          neighbor.bottom = neighbor.bottom ? false : true;
+        }
+      }
+
+      if (side === 'bottom') {
+        neighbor = maze.cells[index(this.column, this.row + 1)];
+        if (neighbor) {
+          this.bottom = this.bottom ? false : true;
+          neighbor.top = neighbor.top ? false : true;
+        }
+      }
+
+
+    };
+// }
+
+// Draw {
+    Cell.prototype.draw = function (ctx) {
+      ctx.lineWidth = 1;
+      var cellWidth = this.maze.cellWidth;
+      var cellHeight = this.maze.cellHeight;
+      if (!this.left) {
+        ctx.strokeStyle = this.leftColor;
+        ctx.beginPath();
+        ctx.moveTo(this.column * cellWidth, this.row * cellHeight);
+        ctx.lineTo(this.column * cellWidth, (this.row + 1) * cellHeight);
+        ctx.closePath();
+        ctx.stroke();
+      }
+      if (!this.right) {
+        ctx.strokeStyle = this.rightColor;
+        ctx.beginPath();
+        ctx.moveTo((this.column + 1) * cellWidth, this.row * cellHeight);
+        ctx.lineTo((this.column + 1)* cellWidth, (this.row + 1) * cellHeight);
+        ctx.closePath();
+        ctx.stroke();
+      }
+      if (!this.top) {
+        ctx.strokeStyle = this.topColor;
+        ctx.beginPath();
+        ctx.moveTo(this.column * cellWidth, this.row * cellHeight);
+        ctx.lineTo((this.column + 1) * cellWidth, this.row * cellHeight);
+        ctx.closePath();
+        ctx.stroke();
+      }
+      if (!this.bottom) {
+        ctx.strokeStyle = this.bottomColor;
+        ctx.beginPath();
+        ctx.moveTo(this.column * cellWidth, (this.row + 1) * cellHeight);
+        ctx.lineTo((this.column + 1) * cellWidth, (this.row + 1) * cellHeight);
+        ctx.closePath();
+        ctx.stroke();
+      }
+      if (this.maze.start === this) {
+        ctx.fillStyle = this.maze.startFill;
+      } else if (this.maze.end === this) {
+        ctx.fillStyle = this.maze.endFill;
+      } else {
+        ctx.fillStyle = this.maze.defaultFill;
+      }
+      ctx.beginPath();
+      ctx.fillRect(this.column * cellWidth + 1, this.row * cellHeight + 1, cellWidth - 2, cellHeight - 2);
+      ctx.closePath();
+
+    };
+
+// }
 
     return Maze;
 
